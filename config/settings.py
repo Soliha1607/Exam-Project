7a35502699981eda78a5a1d234c6049cbfe63486
Soliha1607 <solihapahridinova@gamil.com>
2025-04-22 14:16:52 +0500

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_y6ik=&req_ryzi#3e8cvwv1@8cp%pnthogdr%rrtvr2-h(iep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://exam-project-production-529f.up.railway.app/']
 
 
 # Application definition
